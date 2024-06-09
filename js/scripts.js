32941edbaf9da8250f66ff3c3e481298b6214cb4
andrewsmyth09 <asmyth@gmx.com>
2024-06-09 14:55:26 +0200

    First draft of modal

--- js/scripts.js
+++ js/scripts.js
@@ -17,19 +17,49 @@ async function fetchAPI(url) {
   } catch (error) {
     console.error(error);
   }
-};
+}
 
-const collectCards = () => document.querySelectorAll('.card');
+const collectCards = () => document.querySelectorAll(".card");
+
+const setBirthday = date => {
+  const birthday = new Date(date);
+  return `${birthday.getMonth()}/${birthday.getDate()}/${birthday.getYear()}`;
+}
+
+/* END OF HELPER FUNCTIONS */
 
 async function getUsers() {
   const users = await fetchAPI("https://randomuser.me/api?results=12");
   if (users && users.results) {
-    users.results.forEach((user) => displayInfo(user.name.first, user.name.last, user.email, user.location.city, user.location.country, user.picture.thumbnail));
+    users.results.forEach((user) => {
+      displayInfo(
+        user.name.first,
+        user.name.last,
+        user.email,
+        user.location.city,
+        user.location.country,
+        user.picture.thumbnail
+      );
+
+      displayModal(
+        user.picture.medium,
+        user.name.first,
+        user.name.last,
+        user.email,
+        user.location.city,
+        user.cell,
+        user.location.street.name,
+        user.location.street.number,
+        user.location.country,
+        user.location.postcode,
+        setBirthday(user.dob.date)
+      );
+    });
   }
 }
 
 function displayInfo(firstName, lastName, email, city, country, picture) {
-    const cards = `
+  const cards = `
           <div class="card">
               <div class="card-img-container">
                   <img class="card-img" src="${picture}" alt="profile picture">
@@ -41,9 +71,40 @@ function displayInfo(firstName, lastName, email, city, country, picture) {
               </div>
           </div>
       `;
-        gallery.insertAdjacentHTML("beforeend", cards);
-        cardStack = collectCards();
-       
+  gallery.insertAdjacentHTML("beforeend", cards);
+  cardStack = collectCards();
+}
+
+function displayModal(
+  image,
+  firstName,
+  lastName,
+  email,
+  city,
+  cell,
+  street,
+  streetNumber,
+  country,
+  postcode,
+  birthday
+) {
+  const modal = `
+  <div class="modal-container">
+                <div class="modal">
+                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                    <div class="modal-info-container">
+                        <img class="modal-img" src="${image}" alt="profile picture">
+                        <h3 id="name" class="modal-name cap">${firstName} ${lastName}</h3>
+                        <p class="modal-text">${email}</p>
+                        <p class="modal-text cap">${city}</p>
+                        <hr>
+                        <p class="modal-text">${cell}</p>
+                        <p class="modal-text">${streetNumber} ${street}, ${city}, ${country}, ${postcode}</p>
+                        <p class="modal-text">Birthday: ${birthday}</p>
+                    </div>
+                </div>
+  `;
+  gallery.insertAdjacentHTML("beforeend", modal);
 }
 
 getUsers();
